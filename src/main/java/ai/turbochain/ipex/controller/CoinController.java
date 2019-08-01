81774c9e15f31cb58c5e2f60f6f6b979b33a84eb
yanxingkui <mengdiege@126.com>
2019-08-01 15:58:28 +0800

    新增根据结算币种获取人民币兑换利率

--- src/main/java/ai/turbochain/ipex/controller/CoinController.java
+++ src/main/java/ai/turbochain/ipex/controller/CoinController.java
@@ -2,15 +2,20 @@ package ai.turbochain.ipex.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ai.turbochain.ipex.constant.PageModel;
+import ai.turbochain.ipex.constant.SysConstant;
 import ai.turbochain.ipex.controller.BaseController;
 import ai.turbochain.ipex.entity.Coin;
 import ai.turbochain.ipex.service.CoinService;
 import ai.turbochain.ipex.util.MessageResult;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,11 +27,14 @@ import java.util.Map;
  * @Description: coin
  * @date 2018/4/214:20
  */
+@Slf4j
 @RestController
 @RequestMapping("coin")
 public class CoinController extends BaseController {
     @Autowired
     private CoinService coinService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @GetMapping("legal")
     public MessageResult legal() {
@@ -57,5 +65,24 @@ public class CoinController extends BaseController {
             }
         });
         return result;
+    }
+    
+    
+    @GetMapping("/cny-rate/{symbol}")
+    public MessageResult CoinCnyRate(@PathVariable("symbol") String symbol) {
+    	
+    	String key = SysConstant.DIGITAL_CURRENCY_MARKET_PREFIX + symbol;
+
+    	ValueOperations valueOperations = redisTemplate.opsForValue();
+       
+    	Object bondvalue =valueOperations.get(key);
+          
+        if (bondvalue==null) {
+            log.info(">>>>>>缓存中无利率转换数据>>>>>"+symbol);
+        } else {
+            log.info(symbol+">>>>缓存中利率转换数据>>>"+bondvalue);
+        }
+          
+        return success(bondvalue);
     }
 }

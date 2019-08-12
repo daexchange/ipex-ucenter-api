7ec8c07c474b0af762b7cb190f0647e739b38865
yanxingkui <mengdiege@126.com>
2019-08-12 11:36:19 +0800

    新增注册，快速支付、获取钱包、异步创建钱包接口

---
+++ src/main/java/ai/turbochain/ipex/controller/MobileExangeAssetController.java
@@ -0,0 +1,162 @@
+package ai.turbochain.ipex.controller;
+
+import static org.springframework.util.Assert.hasText;
+import static org.springframework.util.Assert.isTrue;
+import static org.springframework.util.Assert.notNull;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import ai.turbochain.ipex.constant.BooleanEnum;
+import ai.turbochain.ipex.constant.CommonStatus;
+import ai.turbochain.ipex.entity.Coin;
+import ai.turbochain.ipex.entity.Member;
+import ai.turbochain.ipex.entity.MemberWallet;
+import ai.turbochain.ipex.service.CoinService;
+import ai.turbochain.ipex.service.ExangeService;
+import ai.turbochain.ipex.service.LocaleMessageSourceService;
+import ai.turbochain.ipex.service.MemberExclusiveFeeService;
+import ai.turbochain.ipex.service.MemberLevelFeeService;
+import ai.turbochain.ipex.service.MemberService;
+import ai.turbochain.ipex.service.MemberWalletService;
+import ai.turbochain.ipex.util.Md5;
+import ai.turbochain.ipex.util.MessageResult;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/mobile-exange/asset")
+@Slf4j
+public class MobileExangeAssetController {
+	
+	public static final Integer RESULT_FAIL_CODE = 1;
+	
+    @Autowired
+    private MemberService memberService;
+    @Autowired
+    private MemberWalletService memberWalletService;
+    @Autowired
+    private ExangeService exangeService;
+    @Autowired
+    private LocaleMessageSourceService messageSourceService; 
+    @Autowired
+    private CoinService coinService;
+    @Autowired
+    private MemberLevelFeeService memberLevelFeeService;
+    @Autowired
+    private MemberExclusiveFeeService memberExclusiveFeeService;
+    
+    boolean checkJyPassword(Member memberFrom, String jyPassword) throws Exception {
+       
+    	hasText(jyPassword, messageSourceService.getMessage("MISSING_JYPASSWORD"));
+    	
+    	String mbPassword = memberFrom.getJyPassword();
+         
+        Assert.hasText(mbPassword, messageSourceService.getMessage("NO_SET_JYPASSWORD"));
+        Assert.isTrue(Md5.md5Digest(jyPassword + memberFrom.getSalt()).toLowerCase().equals(mbPassword), messageSourceService.getMessage("ERROR_JYPASSWORD"));
+       
+        return true;
+    }
+    
+    
+    /**
+     * 快速支付
+     * @param user
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping("/quick-pay")
+    @Transactional(rollbackFor = Exception.class)
+    public MessageResult quickPay( 
+    		@RequestParam(value = "coinId", required = true) String coinId, 
+    		@RequestParam(value = "payerEmail", required = true) String payerEmail,
+    		@RequestParam(value = "payeeEmail", required = true) String payeeEmail,
+    		@RequestParam(value = "amount", required = true)  BigDecimal amount) throws Exception {
+            
+    	MessageResult messageResult = null;
+    	
+    	try {
+    		if (payerEmail.equals(payeeEmail)) {
+                return new MessageResult(RESULT_FAIL_CODE, "不能转账给本人");
+            }
+    		
+        	// 判断改货币是否支持转账
+        	Coin coin = (Coin) coinService.findOne(coinId);
+        	
+        	notNull(coin, messageSourceService.getMessage("COIN_ILLEGAL"));
+        	
+        	if (coin.getStatus().equals(CommonStatus.ILLEGAL) && BooleanEnum.IS_FALSE.equals(coin.getCanTransfer())) {
+        		return new MessageResult(RESULT_FAIL_CODE, "当前币种不支持转账");
+        	}
+        	
+    		Member memberFrom = memberService.findByEmail(payerEmail);
+    		
+    		if (memberFrom==null) {
+        		// TODO 国际化
+        		return MessageResult.error("该邮件地址尚未注册会员！");
+        	}
+    		
+        	MemberWallet memberWallet = memberWalletService.getMemberWalletByCoinAndMemberId(coinId, memberFrom.getId());
+            
+            isTrue(memberWallet.getIsLock()==BooleanEnum.IS_FALSE,"钱包已锁定");
+
+        	if (memberWallet.getBalance().compareTo(amount) < 0) {
+                return new MessageResult(RESULT_FAIL_CODE, "余额不足");
+            }
+        	
+        	Member memberTo = memberService.findByEmail(payeeEmail);
+        	
+        	if (memberTo==null) {
+        		// TODO 国际化
+        		return MessageResult.error("该邮件地址尚未注册会员！");
+        	}
+
+        	BigDecimal fee =  BigDecimal.ZERO;
+        	
+        	if(fee == null){
+        		return new MessageResult(RESULT_FAIL_CODE, "转账失败！");
+        	}
+        	
+        	// 资金划转
+        	messageResult = exangeService.transferToOther(coinId, amount,fee, memberFrom.getId(), memberTo.getId());
+    	
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    		messageResult = new MessageResult(500,e.getMessage());
+    	}
+    	
+    	return messageResult;
+    }
+   
+   /**
+    * 查询币币账户
+    * @param user
+    * @return
+    */
+   @RequestMapping("/wallet")
+   public MessageResult queryWallet(
+		@RequestParam(value = "memberId", required = true) Long memberId,
+   		@RequestParam(value = "coinId", required = false) String coinId ) {
+		  
+	   MessageResult result = MessageResult.success();
+	  
+	   if (StringUtils.isBlank(coinId)) {
+		   List<MemberWallet>  list = memberWalletService.findAllByMemberId(memberId);
+		   result.setData(list);
+	   } else {
+		   Coin coin = new Coin();
+		   coin.setName(coinId);
+		   MemberWallet memberWallet = memberWalletService.findByCoinAndMemberId(coin, memberId);
+		   result.setData(memberWallet);
+	   }
+       return result;
+   }
+   
+}

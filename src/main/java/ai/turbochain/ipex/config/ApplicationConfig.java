626de0bb4ff9fd86f7e48cee990e5130ce3baf5a
yanxingkui <mengdiege@126.com>
2019-12-11 16:56:57 +0800

    新增HardId服务： 1. 新增通过邮件、手机号注册服务 2. 新增重置密码服务

--- src/main/java/ai/turbochain/ipex/config/ApplicationConfig.java
+++ src/main/java/ai/turbochain/ipex/config/ApplicationConfig.java
@@ -75,9 +75,12 @@ public class ApplicationConfig  extends WebMvcConfigurerAdapter {
         registry.addInterceptor(new MemberInterceptor())
                 .addPathPatterns("/**")
                 .excludePathPatterns("/register/**", "/mobile/code", "/email/code","/email/login/code","/email/reset/code","/getKaptchaImage","/login","/check","/check/login","/start/captcha","/support/country",
-                        "/ancillary/**","/announcement/**","/mobile/reset/code","/reset/email/code","/mobile-register/email","/mobile-register/reset-password","/mobile-exange/asset/wallet/**","/mobile-exange/asset/quick-pay","/reset/login/password","/vote/info","/coin/supported","/financial/items/**","/coin/guess/index","/coin/guess/record"
-                        ,"/coin/guess/detail","/coin/cny-rate/**"
-                        ,"/coin/guess/type");
+                        "/ancillary/**","/announcement/**","/mobile/reset/code","/reset/email/code",
+                        "/mobile-register/email","/mobile-register/reset-password","/mobile-exange/asset/wallet/**","/mobile-exange/asset/quick-pay",
+                        "/reset/login/password","/vote/info","/coin/supported","/financial/items/**","/coin/guess/index","/coin/guess/record",
+                       "/hard-id/register","/hard-id/reset-password",
+                       "/coin/guess/detail","/coin/cny-rate/**","/coin/guess/type");
+                        
         super.addInterceptors(registry);
     }
 
---
+++ src/main/java/ai/turbochain/ipex/entity/HardIdRegister.java
@@ -0,0 +1,42 @@
+package ai.turbochain.ipex.entity;
+
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+
+import lombok.Data;
+
+/**
+ * @author  
+ * @date 2019年8月9日
+ */
+@Data
+public class HardIdRegister {
+
+    @NotBlank(message = "{LoginByEmail.email.null}")
+    @Email(message = "{LoginByEmail.email.format}")
+    private String email;
+
+   // @NotBlank(message = "{LoginByEmail.password.null}")
+    @Length(min = 6, max = 20, message = "{LoginByEmail.password.length}")
+    private String password;
+
+  //  @NotBlank(message = "{LoginByEmail.username.null}")
+  //  @Length(min = 3, max = 20, message = "{LoginByEmail.username.length}")
+   // private String username;
+    
+    /**
+     * 大陆手机号码11位数，匹配格式：前三位固定格式+后8位任意数
+     * 此方法中前三位格式有：
+     * 13+任意数
+     * 15+除4的任意数
+     * 18+任意数
+     * 17+任意数
+     * 147
+     */
+    @Pattern(regexp = "^((13[0-9])|(15[^4])|(18[0-9])|(17[0-9])|(147))\\d{8}$", message = "{LoginByPhone.phone.pattern}")
+    /* @NotBlank(message = "{LoginByPhone.phone.null}")*/
+    private String mobilePhone;
+}

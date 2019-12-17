e9dff1bf96d1ed21cb05a7905c5fec489c3e93fa
李嵩 <lisong@pansoft.com>
2019-12-17 09:12:18 +0800

    添加实体类

---
+++ src/main/java/ai/turbochain/ipex/entity/RespWallet.java
@@ -0,0 +1,16 @@
+package ai.turbochain.ipex.entity;
+
+import lombok.Data;
+
+/**
+ * @author 未央
+ * @create 2019-12-16 16:21
+ */
+@Data
+public class RespWallet {
+
+    private MemberLegalCurrencyWallet wallet;
+
+    private Long otcCoin_id;
+
+}
